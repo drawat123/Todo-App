1dac3132ed331f2d54b126ae44e084afd46d5b6a
divyam <divyamrawat710@gmail.com>
2021-05-29 19:22:47 +0530

    .

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -39,26 +39,42 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith(
+//       (async () => {
+//         try {
+//           const preloadResp = await event.preloadResponse;
 
-          if (preloadResp) {
-            return preloadResp;
-          }
+//           if (preloadResp) {
+//             return preloadResp;
+//           }
 
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.matchAll();
-          console.log('response', cachedResp);
-          return cachedResp;
-        }
-      })()
-    );
-  }
+//           const networkResp = await fetch(event.request);
+//           return networkResp;
+//         } catch (error) {
+//           const cache = await caches.open(CACHE);
+//           const cachedResp = await cache.matchAll();
+//           console.log('response', cachedResp);
+//           return cachedResp;
+//         }
+//       })()
+//     );
+//   }
+// });
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(CACHE);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
 });
