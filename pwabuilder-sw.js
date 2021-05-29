e14c2320e6733b0aa94c1c92009636589148b6cf
divyam <divyamrawat710@gmail.com>
2021-05-29 15:53:31 +0530

    .

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,4 +1,6 @@
-const CACHE = 'pwabuilder-page';
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
+);
 
 const assets = [
   '/',
@@ -14,31 +16,45 @@ const assets = [
   '/Todo-App/icons/android/android-launchericon-512-512.png',
 ];
 
-self.addEventListener('install', (installEvent) => {
-  installEvent.waitUntil(
-    caches
-      .open(CACHE)
-      .then((cache) => {
-        return cache.addAll(assets);
-      })
-      .catch((err) => console.log(err))
+const CACHE = 'pwabuilder-page';
+
+const offlineFallbackPage = ['index.html', 'script.js', 'style.css'];
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(offlineFallbackPage))
   );
 });
 
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
 self.addEventListener('fetch', (event) => {
-  if (event.request.url === 'https://drawat123.github.io/Todo-App/') {
+  if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request).catch((err) =>
-        self.cache
-          .open(cache_name)
-          .then((cache) => cache.match('/Todo-App/index.html'))
-      )
-    );
-  } else {
-    event.respondWith(
-      fetch(event.request).catch((err) =>
-        caches.match(event.request).then((response) => response)
-      )
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+
+          if (preloadResp) {
+            return preloadResp;
+          }
+
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.matchAll(offlineFallbackPage);
+          return cachedResp;
+        }
+      })()
     );
   }
 });
