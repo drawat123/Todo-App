f77818420f669c18af7ac711c68500527d4d213a
divyam <divyamrawat710@gmail.com>
2021-05-29 14:46:11 +0530

    .

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,17 +1,9 @@
-// This is the "Offline page" service worker
-
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
-);
-
 const CACHE = 'pwabuilder-page';
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = 'index.html';
-const offlineFallbackPage = [
+const assets = [
   '/',
   '/Todo-App',
-  '/index.html',
+  '/Todo-App/index.html',
   '/Todo-App/style.css',
   '/Todo-App/script.js',
   '/Todo-App/logo.png',
@@ -22,41 +14,18 @@ const offlineFallbackPage = [
   '/Todo-App/icons/android/android-launchericon-512-512.png',
 ];
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(offlineFallbackPage))
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      cache.addAll(assets);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
-          }
-
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      })()
-    );
-  }
+self.addEventListener('fetch', (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
 });
